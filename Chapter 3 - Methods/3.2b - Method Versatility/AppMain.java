5a2ab3fd74f3d0036b9033e9362969b5a52e1c52
MihirMajgaonkar <mihir.majgaonkar@gmail.com>
2023-10-12 08:34:41 -0700

    working on assignment

--- Chapter 3 - Methods/3.2b - Method Versatility/AppMain.java
+++ Chapter 3 - Methods/3.2b - Method Versatility/AppMain.java
@@ -1,13 +1,32 @@
+
+
 public class AppMain {
     public static void main(String[] args) {
         // Line 1
         // TODO
 
+
         // Line 2
         // TODO
+        returnValue(0, 100);
+    }
+
+    public static int returnValue(double min, double max) {
+        return (int) ((int) (Math.random() * (max-min)) +min);
     }
     
+    public static int returnValue(int x, int y) {
+        double RandomNumber = Math.random();
+        int ConvertedNumber = (int) RandomNumber;
+        System.out.println(ConvertedNumber);
+
+        return ConvertedNumber;
+    }
+
+
     // Put your functions here...
     // Function 1 should: should return a value less than a maximum provided value: [0,𝑚𝑎𝑥𝑉𝑎𝑙𝑢𝑒).
     // Function 2 should: should return a value between two provided doubles: [𝑚𝑖𝑛𝑉𝑎𝑙𝑢𝑒,𝑚𝑎𝑥𝑉𝑎𝑙𝑢𝑒).
 }
+
+
