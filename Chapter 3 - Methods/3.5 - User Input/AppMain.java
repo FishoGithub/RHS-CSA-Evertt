55ae277b384f9784209ee4ad9397ecc327a82341
MihirMajgaonkar <mihir.majgaonkar@gmail.com>
2023-10-19 09:13:07 -0700

    finished user input assignment

--- Chapter 3 - Methods/3.5 - User Input/AppMain.java
+++ Chapter 3 - Methods/3.5 - User Input/AppMain.java
@@ -1,7 +1,40 @@
+import javafx.scene.input.InputEvent;
+
+import java.io.Console;
+import java.util.Scanner;
+
 public class AppMain {
     public static void main(String[] args) {
-        // Create a Scanner object.
-        // Asks the user to type their age, the current year, a future year, and their name.
-        // Print out "<Name> will be <computed age> years old in <future year>".
+        int CalculatedAge = 0;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int UserAge = input.nextInt();
+        System.out.print("Enter the current year: ");
+        int CurrentYear = input.nextInt();
+        System.out.print("Enter a year in the future: ");
+        int FutureYear = input.nextInt();
+        System.out.print("Enter your name: ");
+        String UserName = input.nextLine();
+
+        CalculatedAge = (FutureYear - CurrentYear) + UserAge; 
+
+        System.out.print("\n" + UserName + " will be " + CalculatedAge + " years old in " + FutureYear + ".");
+
+        input.close();
+    }
+
+    //ignore code below, just testing before reading about Scanner
+    public class CalculateUserInputs {
+        public final int USERAGE = 0;
+        public final int CURRENTYEAR = 2023;
+        public final int INPUTFUTUREYEAR = 0;
+        public final String USERNAME = "";
+
+        public int calculateYearFromInput(int CURRENTYEAR, int FUTUREYEAR) {
+            int INPUTFUTUREYEAR = CURRENTYEAR - FUTUREYEAR;
+            return FUTUREYEAR;
+        }
     }
 }
